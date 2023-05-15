76719a03cc9f08ab868ed29f2f5cef97ca1b620f
Paul <avlex82@gmail.com>
2023-05-15 15:52:30 +0200

    Update head URL

--- libsigrok.rb
+++ libsigrok.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Libsigrok < Formula
   url 'http://sigrok.org/download/source/libsigrok/libsigrok-0.5.0.tar.gz'
   homepage 'http://sigrok.org/'
-  head 'https://github.com/helgames/libsigrok.git',:using => Class.new(GitDownloadStrategy) { def support_depth?; false; end }
+  head 'https://github.com/helindustries/libsigrok.git',:using => Class.new(GitDownloadStrategy) { def support_depth?; false; end }
   sha256 '4c8c86779b880a5c419f6c77a08b1147021e5a19fa83b0f3b19da27463c9f3a4'
 
   depends_on 'libzip'
